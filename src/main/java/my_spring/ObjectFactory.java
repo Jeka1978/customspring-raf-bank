f28cc29188577396f048a7c689e4dcbaaa3a8036
jeka1978 <kit2009>
2019-06-14 11:05:51 +0300

    add @injectRandomInt Support

--- src/main/java/my_spring/ObjectFactory.java
+++ src/main/java/my_spring/ObjectFactory.java
@@ -1,10 +1,15 @@
 package my_spring;
 
+import lombok.SneakyThrows;
+
+import java.lang.reflect.Field;
+
 /**
  * @author Evgeny Borisov
  */
 public class ObjectFactory {
     private static ObjectFactory ourInstance = new ObjectFactory();
+    private Config config = new JavaConfig();
 
     public static ObjectFactory getInstance() {
         return ourInstance;
@@ -14,22 +19,16 @@ public class ObjectFactory {
     }
 
 
+    @SneakyThrows
     public <T> T createObject(Class<T> type) {
-        
+        if (type.isInterface()) {
+            type = config.getImplClass(type);
+        }
+        T t = type.newInstance();
+
+        return t;
+
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
